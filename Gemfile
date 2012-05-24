345b38e4a019693ae0d8121559a47f357a2dca7a
Gene Awot <easyable01@gmail.com>
2012-05-24 11:38:31 -0400

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -1,18 +1,17 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
-
+gem 'bootstrap-sass', '2.0.0'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 group :development, :test do
   gem 'sqlite3'
-  gem 'rspec-rails', '2.9.0'
+  gem 'rspec-rails', '2.10.0'
 end
-
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
+  gem 'sass-rails', '3.2.4'
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
   
-  def full_title(page_title) #Returns full title on a per page basis  #method definition
+  def full_title(page_title) #Returns full title on a per-page basis  #method definition
     base_title = "Ruby on Rails Tutorial Sample App1"                 #Variable assignment
-  	if                                                                #begin if statement
-  		page_title.empty?                                               # boolean test
-  	   base_title                                                     #implicit return
+  	if page_title.empty?                                               # boolean test
+  	  base_title                                                                   #begin if statement
   	else                                                              #else
       "#{base_title} | #{page_title}"                                 # string interpolation
     end                                                               # end if statemtent
